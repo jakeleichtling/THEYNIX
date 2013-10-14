21016669d022bbb65821c29b00bfe31226a43d17
Jake Leichtling <jake.leichtling@gmail.com>
2013-10-13 22:44:07 -0400

    Filling in Kernel and PMem files.

--- Kernel.c
+++ Kernel.c
@@ -1,12 +1,31 @@
+#include <assert.h>
+
 #include "Kernel.h"
 
 void SetKernelData(void *_KernelDataStart, void *_KernelDataEnd) {
-    kernel_brk_page = UP_TO_PAGE(_KernelDataEnd);
-    kernel_data_start_page = DOWN_TO_PAGE(_KernelDataStart);
+    kernel_brk_page = ADDR_TO_PAGE(((unsigned int) _KernelDataEnd) - 1) + 1;
+    kernel_data_start_page = ADDR_TO_PAGE(_KernelDataStart);
+    kernel_text_end_page = ADDR_TO_PAGE(((unsigned int) _KernelDataStart) - 1);
 }
 
 void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt) {
-    // Initialize data structures in kernel.h, including used_frames
+    // Initialize the kernel's data structures.
+    locks = ListNewList();
+    cvars = ListNewList();
+    pipes = ListNewList();
+
+    ttys = malloc(NUM_TERMINALS * sizeof(TTY));
+    int i;
+    for (i = 0; i < NUM_TERMINALS; i++) {
+        TtyInit(ttys[i]);
+    }
+
+    //current_proc =
+    ready_queue = ListNewList();
+    clock_block_procs = ListNewList();
+
+    unused_frames = NewUnusedFrames(pmem_size);
+    virtual_memory_enabled = false;
 
     // Initialize the interrupt vector and point to it in REG_VECTOR_BASE
 
@@ -15,12 +34,12 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt) {
     // REG_PTLR0, REG_PTBR1, and REG_PTLR1
 
     // Enable virtual memory. Wooooo!
+    WriteRegister(REG_VM_ENABLE, 1);
+    virtual_memory_enabled = true;
 
     // Create the idle process and put it on the ready queue.
 
     // Create the first process (see template.c) and load the initial program into it.
-
-    // Return!
 }
 
 int SetKernelBrk(void *addr) {
