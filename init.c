3b6f2c3d81637e4146bb456035e601e6b3be8601
Jake Leichtling <jake.leichtling@gmail.com>
2013-10-22 00:52:56 -0400

    .

--- init.c
+++ init.c
@@ -1,10 +1,11 @@
+#include <hardware.h>
 #include <stdbool.h>
 
 #include "Log.h"
 
 int main(int argc, char *argv[]) {
      while (true) {
-        TracePrintf(TRACE_LEVEL_DETAIL_INFO, "Pause loop in main()!")
+        TracePrintf(TRACE_LEVEL_DETAIL_INFO, "Pause loop in main()!");
         Pause();
     }
 }
