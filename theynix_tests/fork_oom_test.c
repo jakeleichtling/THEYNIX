a75a98d9f19016c45a0668276dbdc3ff703aa53b
Jake Leichtling <jake.leichtling@gmail.com>
2013-11-20 16:54:14 -0500

    .

--- theynix_tests/fork_oom_test.c
+++ theynix_tests/fork_oom_test.c
@@ -32,10 +32,14 @@ int main(int argc, char *argv[]) {
     TtyPrintf(1, "My child returned. Hurray!\n");
 
     // If I'm the init process, do it again, one more time!
+    TtyPrintf(1, "GetPid().\n");
     if (GetPid() == 1) {
+        TtyPrintf(1, "Fork().\n");
         rc = Fork();
+        TtyPrintf(1, "Fork() returned %d.\n", rc);
         if (rc == 0) { // Child process
             // Exec recursively
+            TtyPrintf(1, "Exec().\n");
             rc = Exec("theynix_tests/fork_oom_test", NULL);
             if (rc < 0) {
                 TtyPrintf(1, "Exec failed :(\n");
