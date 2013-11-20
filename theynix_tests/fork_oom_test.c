a19cc25b58ba251924a632e56bfb14f18519ca16
Jake Leichtling <jake.leichtling@gmail.com>
2013-11-20 15:27:46 -0500

    .

--- theynix_tests/fork_oom_test.c
+++ theynix_tests/fork_oom_test.c
@@ -6,6 +6,8 @@
 #include <yalnix.h>
 
 int main(int argc, char *argv[]) {
+    char buffer[12345];
+
     int rc = Fork();
     if (rc == 0) { // Child process
         // Exec recursively
@@ -23,6 +25,22 @@ int main(int argc, char *argv[]) {
     int status;
     Wait(&status);
     TtyPrintf(1, "My child returned. Hurray!\n");
+
+    // If I'm the init process, do it again, one more time!
+    if (GetPid() == 1) {
+        rc = Fork();
+        if (rc == 0) { // Child process
+            // Exec recursively
+            rc = Exec("theynix_tests/fork_oom_test", NULL);
+            if (rc < 0) {
+                TtyPrintf(1, "Exec failed :(\n");
+            }
+        }
+
+        if (rc < 0) {
+            TtyPrintf(1, "Fork failed! How will I start anew?");
+        }
+    }
 
     return 0;
 }
