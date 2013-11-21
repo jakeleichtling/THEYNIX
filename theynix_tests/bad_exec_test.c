91f44967aced429a643af354615d6f000721bc0d
Jake Leichtling <jake.leichtling@gmail.com>
2013-11-21 11:43:33 -0500

    .

---
+++ theynix_tests/bad_exec_test.c
@@ -0,0 +1,26 @@
+/*
+  Tests that the OS will properly handle bad input to Exec, such as
+  an illegal filename address, an illegal argvec address, an illegal
+  address in argvec, and a bad filename.
+*/
+
+#include <stdlib.h>
+#include <yalnix.h>
+
+void main(int argc, char **argv) {
+    // Bad filename
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Exec-ing with a bad filename...\n");
+    int rc = Exec("bogus_file_name.bogus_bogus_bogus", NULL);
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
+
+    // Illegal filename address
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Exec-ing with an illegal filename address...\n");
+    int rc = Exec(char *(0), NULL);
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
+
+    // Illegal argvec address
+
+    // Illegal address in argvec
+
+    return 0;
+}
