d75a08c44d03377633d14d5ab01373d350f8b05a
Jake Leichtling <jake.leichtling@gmail.com>
2013-11-21 12:58:10 -0500

    .

--- theynix_tests/brk_test.c
+++ theynix_tests/brk_test.c
@@ -12,13 +12,18 @@ int main(int argc, char *argv[]) {
     int buffer[1024];
 
     // Try to set the brk in the stack.
-    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Brking into the stack.\n");
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Brk-ing into the stack.\n");
     int rc = Brk(buffer);
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
 
-    // Trying to malloc more than the amount of physical memory available.
-    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Brking close to the stack.\n");
+    // Brk near the stack.
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Brk-ing close to the stack.\n");
     rc = Brk(buffer - 4096);
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
+
+    // Now Brk down a bit.
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Now Brki-ng down a bit.\n");
+    rc = Brk(buffer - 4096 * 2);
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
 
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "All done!\n");
