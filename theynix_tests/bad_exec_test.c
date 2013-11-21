5c0a33b79b164a87133a8a9bd179edab7a671323
Jake Leichtling <jake.leichtling@gmail.com>
2013-11-21 11:52:47 -0500

    .

--- theynix_tests/bad_exec_test.c
+++ theynix_tests/bad_exec_test.c
@@ -23,10 +23,17 @@ int main(int argc, char **argv) {
 
     // Illegal argvec address
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Exec-ing with an illegal argvec address...\n");
-    rc = Exec("theynix_tests/bad_exec_test", (char **) 0);
+    rc = Exec("theynix_tests/bad_exec_test", (char **) 1);
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
 
     // Illegal address in argvec
+    char **argvec = calloc(2, sizeof(char *));
+    argvec[0] = (char *) 1;
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Exec-ing with an illegal address in argvec...\n");
+    rc = Exec("theynix_tests/bad_exec_test", argvec);
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "\t--> returned %d\n", rc);
+
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "If we got to here, all tests likely passed!\n");
 
     return 0;
 }
