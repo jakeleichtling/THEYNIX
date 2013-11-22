b8ff3ee225d351de8b6d2ca0f164cb1f0e3477cf
Jake Leichtling <jake.leichtling@gmail.com>
2013-11-22 14:02:03 -0500

    .

--- theynix_tests/io_test.c
+++ theynix_tests/io_test.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[]) {
     // Test normal read behavior.
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing normal read behavior.\n");
     char *user_message = calloc(TERMINAL_MAX_LINE, sizeof(char));
-    TtyWrite(0, "Please input a message\n", TERMINAL_MAX_LINE);
-    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "About to call TtyRead()");
+    TtyWrite(0, "Please input a message\n\0", TERMINAL_MAX_LINE);
+    TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "About to call TtyRead()\n");
     int input_length = TtyRead(0, user_message, TERMINAL_MAX_LINE);
     TtyWrite(0, user_message, input_length);
 
@@ -37,54 +37,54 @@ int main(int argc, char *argv[]) {
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing writing with an invalid buffer.\n");
     int failure_code = TtyWrite(0, NULL, TERMINAL_MAX_LINE);
     char *failure_test_message = calloc(128, sizeof(char));
-    sprintf(failure_test_message, "Tried to write null. rc = %d\n", failure_code);
+    sprintf(failure_test_message, "Tried to write null. rc = %d\n\0", failure_code);
     TtyWrite(0, failure_test_message, 128);
 
     // Test reading with invalid (null) buffer.
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing reading with an invalid buffer.\n");
     failure_code = TtyRead(0, NULL, TERMINAL_MAX_LINE);
     bzero(failure_test_message, 128);
-    sprintf(failure_test_message, "Tried to read into null buffer. rc = %d\n", failure_code);
+    sprintf(failure_test_message, "Tried to read into null buffer. rc = %d\n\0", failure_code);
     TtyWrite(0, failure_test_message, 128);
 
     // Test writing with id < 0
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing writing with id < 0.\n");
     failure_code = TtyWrite(-1, big_buffer, TERMINAL_MAX_LINE);
     bzero(failure_test_message, 128);
-    sprintf(failure_test_message, "Tried to write with id < 0. rc = %d\n", failure_code);
+    sprintf(failure_test_message, "Tried to write with id < 0. rc = %d\n\0", failure_code);
     TtyWrite(0, failure_test_message, 128);
 
     // Test reading with id < 0
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing reading with id < 0.\n");
     failure_code = TtyRead(-1, big_buffer, TERMINAL_MAX_LINE);
     bzero(failure_test_message, 128);
-    sprintf(failure_test_message, "Tried to read with id < 0. rc = %d\n", failure_code);
+    sprintf(failure_test_message, "Tried to read with id < 0. rc = %d\n\0", failure_code);
     TtyWrite(0, failure_test_message, 128);
 
     // Test writing with id > 3
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing writing with id > 3.\n");
     failure_code = TtyWrite(4, big_buffer, TERMINAL_MAX_LINE);
     bzero(failure_test_message, 128);
-    sprintf(failure_test_message, "Tried to write with id > 3. rc = %d\n", failure_code);
+    sprintf(failure_test_message, "Tried to write with id > 3. rc = %d\n\0", failure_code);
     TtyWrite(0, failure_test_message, 128);
 
     // Test reading with id > 3
     TracePrintf(TRACE_LEVEL_TESTING_OUTPUT, "Testing reading with id > 3.\n");
     failure_code = TtyRead(4, big_buffer, TERMINAL_MAX_LINE);
     bzero(failure_test_message, 128);
-    sprintf(failure_test_message, "Tried to read with id > 3. rc = %d\n", failure_code);
+    sprintf(failure_test_message, "Tried to read with id > 3. rc = %d\n\0", failure_code);
     TtyWrite(0, failure_test_message, 128);
 
     // Test having two writers at the same time.
     int rc = Fork();
     if (0 == rc) {
         bzero(user_message, TERMINAL_MAX_LINE);
-        TtyWrite(0, "Now I (the child) will read!\n", TERMINAL_MAX_LINE);
+        TtyWrite(0, "Now I (the child) will read!\n\0", TERMINAL_MAX_LINE);
         input_length = TtyRead(0, user_message, TERMINAL_MAX_LINE);
         TtyWrite(0, user_message, input_length);
         exit(0);
     }
-    TtyWrite(0, "I'm trying to print at the same time as my child!\n", TERMINAL_MAX_LINE);
+    TtyWrite(0, "I'm trying to print at the same time as my child!\n\0", TERMINAL_MAX_LINE);
     int status;
     Wait(&status); // Let child die first
     return 0;
